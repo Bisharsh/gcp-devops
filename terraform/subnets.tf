62979fc09b35c65552b5449ccb71ca005dee013b
Harshit Bisht <harshitbisht@Harshits-MacBook-Air.local>
2025-01-04 20:47:43 +0100

    updated subnets.tf

---
+++ terraform/subnets.tf
@@ -0,0 +1,18 @@
+resource "google_compute_network" "gke_subnet" {
+	name 						= gke_subnet
+	ip_cidr_range 				= 10.0.0.0/18
+	region 						= "us-central-1"
+	network 					= "google_compute_network.gke_vpc.id"
+	private_ip_google_access	= true
+	
+	secondary_ip_range {
+		range_name 		= "k8s-pods-range"
+		ip_cidr_range 	= "10.48.0.0/14"
+	}
+	
+	secondary_ip_range {
+		range_name 		= "k8s-services-range"
+		ip_cidr_range	= "10.52.0.0/20"
+	}
+}
+
