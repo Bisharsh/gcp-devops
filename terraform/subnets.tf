b906069e2874838a5a94bb60a30f3d6fa1a8d83d
Harshit Bisht <harshitbisht@Harshits-MacBook-Air.local>
2025-01-08 14:37:32 +0100

    fixed errors in terraform files

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,18 +1,17 @@
 resource "google_compute_subnetwork" "gke_subnet" {
-	name 						= "gke_subnet"
-	ip_cidr_range 				= 10.0.0.0/18
-	region 						= "us-central1"
-	network 					= google_compute_network.gke_vpc.id
-	private_ip_google_access	= true
-	
-	secondary_ip_range {
-		range_name 		= "k8s-pods-range"
-		ip_cidr_range 	= "10.48.0.0/14"
-	}
-	
-	secondary_ip_range {
-		range_name 		= "k8s-services-range"
-		ip_cidr_range	= "10.52.0.0/20"
-	}
-}
+  name                      = "gke_subnet"
+  ip_cidr_range             = "10.0.0.0/18"
+  region                    = "us-central1"
+  network                   = google_compute_network.gke_vpc.id
+  private_ip_google_access  = true
 
+  secondary_ip_range {
+    range_name    = "k8s-pods-range"
+    ip_cidr_range = "10.48.0.0/14"
+  }
+
+  secondary_ip_range {
+    range_name    = "k8s-services-range"
+    ip_cidr_range = "10.52.0.0/20"
+  }
+}
